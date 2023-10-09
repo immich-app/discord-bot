ccc67a71e182bffe68437baaf5b579d2f57470e7
Daniel Dietzler <mail@ddietzler.dev>
2023-10-09 15:18:01 +0200

    link to github PRs and issues using #num, suppress github embeds

--- src/commands/slashes.ts
+++ src/commands/slashes.ts
@@ -1,23 +1,22 @@
 import { ApplicationCommandOptionType, MessageFlags, type CommandInteraction } from 'discord.js';
 import { Discord, Slash, SlashChoice, SlashOption } from 'discordx';
-
-const docsDomain = 'https://docs.immich.app/docs';
+import { DOCS_DOMAIN, GITHUB_DOMAIN, GITHUB_API_DOMAIN } from '../constants.js';
 
 const linkCommands: Record<string, string> = {
-  'reverse proxy': `${docsDomain}/administration/reverse-proxy`,
-  database: `${docsDomain}/guides/database-queries`,
-  upgrade: `${docsDomain}/install/docker-compose#step-4---upgrading`,
-  libraries: `${docsDomain}/features/libraries`,
-  sidecar: `${docsDomain}/features/xmp-sidecars`,
-  docker: `${docsDomain}/guides/docker-help`,
-  backup: `${docsDomain}/administration/backup-and-restore`,
-  github: 'https://github.com/immich-app/immich',
-  cli: `${docsDomain}/features/bulk-upload`,
+  'reverse proxy': `${DOCS_DOMAIN}/administration/reverse-proxy`,
+  database: `${DOCS_DOMAIN}/guides/database-queries`,
+  upgrade: `${DOCS_DOMAIN}/install/docker-compose#step-4---upgrading`,
+  libraries: `${DOCS_DOMAIN}/features/libraries`,
+  sidecar: `${DOCS_DOMAIN}/features/xmp-sidecars`,
+  docker: `${DOCS_DOMAIN}/guides/docker-help`,
+  backup: `${DOCS_DOMAIN}/administration/backup-and-restore`,
+  github: GITHUB_DOMAIN,
+  cli: `${DOCS_DOMAIN}/features/bulk-upload`,
 };
 const helpTexts: Record<string, string> = {
   'help ticket':
     'Please open a <#1049703391762321418> ticket with more information and we can help you troubleshoot the issue.',
-  'reverse proxy': `This sounds like it could be a reverse proxy issue. Here's a link to the relevant documentation page: ${docsDomain}/administration/reverse-proxy.`,
+  'reverse proxy': `This sounds like it could be a reverse proxy issue. Here's a link to the relevant documentation page: ${DOCS_DOMAIN}/administration/reverse-proxy.`,
   'feature request':
     "For ideas or features you'd like Immich to have, feel free to [open a feature request in the Github discussions](https://github.com/immich-app/immich/discussions/new?category=feature-request). However, please make sure to search for similar requests first to avoid duplicates. ",
 };
@@ -75,7 +74,7 @@ export class Commands {
     const lastStarsCount = _star_history[interaction.channelId];
 
     try {
-      const response = await (await fetch('https://api.github.com/repos/immich-app/immich')).json();
+      const response = await (await fetch(GITHUB_API_DOMAIN)).json();
       const starsCount = response['stargazers_count'] as number;
       const delta = lastStarsCount && starsCount - lastStarsCount;
       const formattedDelta = delta && Intl.NumberFormat(undefined, { signDisplay: 'always' }).format(delta);
@@ -97,7 +96,7 @@ export class Commands {
     const lastForksCount = _fork_history[interaction.channelId];
 
     try {
-      const response = await (await fetch('https://api.github.com/repos/immich-app/immich')).json();
+      const response = await (await fetch(GITHUB_API_DOMAIN)).json();
       const forksCount = response['forks_count'] as number;
       const delta = lastForksCount && forksCount - lastForksCount;
       const formattedDelta = delta && Intl.NumberFormat(undefined, { signDisplay: 'always' }).format(delta);
