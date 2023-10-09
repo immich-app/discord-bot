c8bdcc4bad8dc92a69491ee66b232657f7396803
bo0tzz <git@bo0tzz.me>
2023-10-09 21:52:15 +0200

    Hide all immich org links (#8)

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const DOCS_DOMAIN = 'https://docs.immich.app/docs';
-export const GITHUB_DOMAIN = 'https://github.com/immich-app/immich';
+export const GITHUB_DOMAIN = 'https://github.com';
+export const IMMICH_REPOSITORY = `${GITHUB_DOMAIN}/immich-app/immich`;
 export const GITHUB_API_DOMAIN = 'https://api.github.com/repos/immich-app/immich';
