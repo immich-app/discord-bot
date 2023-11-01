d52002566711b48b88a30e6429ff477b50dfbb1e
Daniel Dietzler <mail@ddietzler.dev>
2023-11-01 23:09:35 +0100

    fix suppression of embeds

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
-export const IMMICH_DOMAIN = 'https://docs.immich.app';
+export const IMMICH_DOMAIN = 'https://immich.app';
+export const IMMICH_DOCS_DOMAIN = 'https://docs.immich.app';
 export const DOCS_DOMAIN = `${IMMICH_DOMAIN}/docs`;
 export const GITHUB_DOMAIN = 'https://github.com';
 export const IMMICH_REPOSITORY = `${GITHUB_DOMAIN}/immich-app/immich`;
