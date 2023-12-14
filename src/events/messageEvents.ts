88bb3ce37725b40bcd29e7161bac305e0a609475
Daniel Dietzler <mail@ddietzler.dev>
2023-12-14 21:34:45 +0100

    refactor

--- src/events/messageEvents.ts
+++ src/events/messageEvents.ts
@@ -48,40 +48,48 @@ export class MessageEvents {
     const matches = content.matchAll(/(^|\W)#(?<id>[0-9]+)/g);
     const links = new Set<string>();
     for (const match of matches) {
-      if (match?.groups) {
-        const id = match.groups.id;
+      const id = match?.groups?.id;
+      if (!id) {
+        continue;
+      }
 
-        if (Number(id) < 500 || Number(id) > 15000) {
-          continue;
-        }
+      if (Number(id) < 500 || Number(id) > 15000) {
+        continue;
+      }
 
-        try {
-          const response = await octokit.rest.issues.get({
-            ...IMMICH_REPOSITORY_BASE_OPTIONS,
-            issue_number: Number(id),
-          });
+      const link = (await this.getIssueOrPr(id)) || (await this.getDiscussion(id));
 
-          const type = response.data.pull_request ? 'PR' : 'ISSUE';
-          links.add(`[${type}] ${response.data.title} ([#${id}](${response.data.html_url}))`);
-          continue;
-        } catch (error) {
-          if (error instanceof RequestError && error.status !== 404) {
-            console.log(`Could not fetch #${id}`);
-            continue;
-          }
-        }
+      link && links.add(link);
+    }
 
-        try {
-          const { status: discussionStatus } = await fetch(`${Constants.Urls.Discussions}/${id}}`);
-          if (discussionStatus === 200) {
-            links.add(`[Discussion] ([#${id}](${Constants.Urls.Discussions}/${id}))`);
-          }
-        } catch (error) {
-          console.log(`Could not fetch #${id}`);
-          continue;
-        }
+    return [...links];
+  }
+
+  private async getIssueOrPr(id: string) {
+    try {
+      const response = await octokit.rest.issues.get({
+        ...IMMICH_REPOSITORY_BASE_OPTIONS,
+        issue_number: Number(id),
+      });
+
+      const type = response.data.pull_request ? 'Pull Request' : 'Issue';
+      return `[${type}] ${response.data.title} ([#${id}](${response.data.html_url}))`;
+    } catch (error) {
+      if (error instanceof RequestError && error.status !== 404) {
+        console.log(`Could not fetch #${id}`);
       }
     }
-    return [...links];
+  }
+
+  private async getDiscussion(id: string) {
+    try {
+      const { status } = await fetch(`${Constants.Urls.Discussions}/${id}}`);
+
+      if (status === 200) {
+        return `[Discussion] ([#${id}](${Constants.Urls.Discussions}/${id}))`;
+      }
+    } catch (error) {
+      console.log(`Could not fetch #${id}`);
+    }
   }
 }
