fc4babc6e4dbc046f97897212e7c9e8f65a8112e
Daniel Dietzler <36593685+danieldietzler@users.noreply.github.com>
2023-12-14 21:42:06 +0100

    fix: github discussions references (#20)
    * fix github discussions references
    
    * refactor

--- src/events/messageEvents.ts
+++ src/events/messageEvents.ts
@@ -3,6 +3,7 @@ import { IMMICH_REPOSITORY_BASE_OPTIONS, Constants } from '../constants.js';
 import { Message, MessageFlags, PartialMessage } from 'discord.js';
 import { ArgsOf, Discord, On } from 'discordx';
 import _ from 'lodash';
+import { RequestError } from '@octokit/request-error';
 
 const PREVIEW_BLACKLIST = [Constants.Urls.Immich, Constants.Urls.GitHub];
 const octokit = new Octokit();
@@ -47,40 +48,48 @@ export class MessageEvents {
     const matches = content.matchAll(/(^|\W)#(?<id>[0-9]+)/g);
     const links = new Set<string>();
     for (const match of matches) {
-      if (match?.groups) {
-        const id = match.groups.id;
+      const id = match?.groups?.id;
+      if (!id) {
+        continue;
+      }
 
-        if (Number(id) < 500 || Number(id) > 15000) {
-          continue;
-        }
+      if (Number(id) < 500 || Number(id) > 15000) {
+        continue;
+      }
 
-        try {
-          const response = await octokit.rest.issues.get({
-            ...IMMICH_REPOSITORY_BASE_OPTIONS,
-            issue_number: Number(id),
-          });
+      const link = (await this.getIssueOrPr(id)) || (await this.getDiscussion(id));
 
-          if (response.status === 200) {
-            const type = response.data.pull_request ? 'PR' : 'ISSUE';
-            links.add(`[${type}] ${response.data.title} ([#${id}](${response.data.html_url}))`);
-            continue;
-          }
-        } catch (error) {
-          console.log(`Could not fetch #${id}`);
-          continue;
-        }
+      link && links.add(link);
+    }
 
-        try {
-          const { status: discussionStatus } = await fetch(`${Constants.Urls.Discussions}/${id}}`);
-          if (discussionStatus === 200) {
-            links.add(`[Discussion] ([#${id}](${Constants.Urls.Discussions}/${id}))`);
-          }
-        } catch (error) {
-          console.log(`Could not fetch #${id}`);
-          continue;
-        }
+    return [...links];
+  }
+
+  private async getIssueOrPr(id: string) {
+    try {
+      const response = await octokit.rest.issues.get({
+        ...IMMICH_REPOSITORY_BASE_OPTIONS,
+        issue_number: Number(id),
+      });
+
+      const type = response.data.pull_request ? 'Pull Request' : 'Issue';
+      return `[${type}] ${response.data.title} ([#${id}](${response.data.html_url}))`;
+    } catch (error) {
+      if (error instanceof RequestError && error.status !== 404) {
+        console.log(`Could not fetch #${id}`);
       }
     }
-    return [...links];
+  }
+
+  private async getDiscussion(id: string) {
+    try {
+      const { status } = await fetch(`${Constants.Urls.Discussions}/${id}}`);
+
+      if (status === 200) {
+        return `[Discussion] ([#${id}](${Constants.Urls.Discussions}/${id}))`;
+      }
+    } catch (error) {
+      console.log(`Could not fetch #${id}`);
+    }
   }
 }
