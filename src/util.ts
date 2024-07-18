19ca3c9af04481897ec242aa66f9bfc68989b198
Zack Pollard <zackpollard@ymail.com>
2024-07-18 19:20:46 +0100

    feat: add payment totals to discord messages

---
+++ src/util.ts
@@ -0,0 +1,13 @@
+import { Constants } from './constants';
+import type { TextChannel } from 'discord.js';
+import { Client } from 'discordx';
+
+export const logError = async (message: string, error: unknown, bot: Client) => {
+  console.error(message, error);
+  try {
+    const botSpamChannel = (await bot.channels.fetch(Constants.Channels.BotSpam)) as TextChannel;
+    await botSpamChannel.send(`${message}: ${error}`);
+  } catch (error) {
+    console.error('Failed to send error message to bot spam channel', error);
+  }
+};
