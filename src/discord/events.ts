7bba06c5cbed76d9e627561c358134b483dce5b2
Jason Rasmussen <jason@rasm.me>
2024-08-02 14:58:40 -0400

    refactor: blacklist logic

--- src/discord/events.ts
+++ src/discord/events.ts
@@ -1,11 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Message, MessageFlags, PartialMessage } from 'discord.js';
+import { MessageFlags } from 'discord.js';
 import { ArgsOf, Discord, On, Once } from 'discordx';
 import _ from 'lodash';
-import { Constants } from 'src/constants';
 import { DiscordService } from 'src/services/discord.service';
-
-const PREVIEW_BLACKLIST = [Constants.Urls.Immich, Constants.Urls.GitHub, Constants.Urls.MyImmich];
 
 const shorten = (message: string | null) => {
   if (!message) {
@@ -41,7 +38,10 @@ export class DiscordEvents {
       return;
     }
 
-    await Promise.all([this.handleGithubShortLinks(message)]);
+    const links = await this.service.handleGithubReferences(message.content);
+    if (links.length !== 0) {
+      await message.reply({ content: links.join('\n'), flags: [MessageFlags.SuppressEmbeds] });
+    }
   }
 
   @On({ event: 'messageUpdate' })
@@ -55,7 +55,10 @@ export class DiscordEvents {
 
     if (!_.isEqual(oldMessage.embeds, newMessage.embeds)) {
       this.logger.verbose('Removing embeds', oldMessage.embeds, newMessage.embeds);
-      await this.handlePreventEmbeddings(newMessage);
+      const urls = newMessage.embeds.map((embed) => embed.url).filter((url): url is string => !!url);
+      if (await this.service.hasBlacklistUrl(urls)) {
+        await newMessage.suppressEmbeds(true);
+      }
     } else {
       this.logger.verbose('Skipping, no embeds');
     }
@@ -66,18 +69,5 @@ export class DiscordEvents {
     this.logger.verbose(
       `DiscordBot.onMessageDelete [${message.author?.username || 'Unknown'}] => ${shorten(message.content)}`,
     );
-  }
-
-  private async handleGithubShortLinks(message: Message<boolean>) {
-    const links = await this.service.handleGithubReferences(message.content);
-    if (links.length !== 0) {
-      await message.reply({ content: links.join('\n'), flags: [MessageFlags.SuppressEmbeds] });
-    }
-  }
-
-  private async handlePreventEmbeddings(message: Message<boolean> | PartialMessage) {
-    if (message.embeds.find((embed) => PREVIEW_BLACKLIST.find((domain) => embed.url?.startsWith(domain)))) {
-      await message.suppressEmbeds(true);
-    }
   }
 }
