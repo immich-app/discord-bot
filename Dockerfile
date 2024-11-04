3931853d9d97c70cc89e53b0021e3da310a39036
Daniel Dietzler <36593685+danieldietzler@users.noreply.github.com>
2024-11-04 20:03:03 +0100

    chore: esm imports! (#121)

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:lts-alpine@sha256:c13b26e7e602ef2f1074aef304ce6e9b7dd284c419b35d89fcf3cc8e44a8def9 AS build-runner
+FROM node:22.11.0-alpine AS build-runner
 
 WORKDIR /app
 COPY package*.json .
@@ -8,7 +8,7 @@ COPY tsconfig.json .
 RUN npm run build
 RUN npm prune --omit=dev
 
-FROM node:lts-alpine@sha256:c13b26e7e602ef2f1074aef304ce6e9b7dd284c419b35d89fcf3cc8e44a8def9
+FROM node:22.11.0-alpine
 
 RUN apk add --no-cache tini
 WORKDIR /app
--- src/services/oauth.service.ts
+++ src/services/oauth.service.ts
@@ -1,19 +1,11 @@
 import { BadRequestException, Inject, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
-import { Client, Issuer, generators } from 'openid-client';
+// @ts-expect-error f'ing ts does not let you import types from esm
+import type { Configuration, ServerMetadata } from 'openid-client';
+// @ts-expect-error we have the experimental flag enabled so we can import esm packages
+import client from 'openid-client';
 import { getConfig } from 'src/config';
 import { OAuthAuthorizeDto, OAuthCallbackDto } from 'src/dtos/oauth.dto';
 import { IDatabaseRepository } from 'src/interfaces/database.interface';
-
-type GithubProfile = {
-  login: string;
-  id: string;
-  avatar_url: string;
-  url: string;
-  type: 'User';
-  name: string;
-  created_at: string;
-  updated_at: string;
-};
 
 type StateItem = { value: string; expiresAt: number };
 const stateMap = new Map<string, StateItem>();
@@ -21,28 +13,25 @@ const stateMap = new Map<string, StateItem>();
 @Injectable()
 export class OAuthService {
   private logger: Logger = new Logger(OAuthService.name);
-  private client: Client;
+  private config: Configuration;
 
   constructor(@Inject(IDatabaseRepository) private database: IDatabaseRepository) {
-    const issuer = new Issuer({
+    const { github } = getConfig();
+    const server: ServerMetadata = {
       issuer: 'https://github.com',
       authorization_endpoint: 'https://github.com/login/oauth/authorize',
       token_endpoint: 'https://github.com/login/oauth/access_token',
       userinfo_endpoint: 'https://api.github.com/user',
-    });
+    };
 
-    const { github } = getConfig();
-    this.client = new issuer.Client({
-      client_id: github.clientId,
-      client_secret: github.clientSecret,
-    });
+    this.config = new client.Configuration(server, github.clientId, github.clientSecret);
   }
 
   authorize(dto: OAuthAuthorizeDto) {
-    const state = generators.state();
-    stateMap.set(state, { value: state, expiresAt: Date.now() + 5 * 60 * 1000 });
+    const state = client.randomState();
+    stateMap.set(dto.redirectUri, { value: state, expiresAt: Date.now() + 5 * 60 * 1000 });
     return {
-      url: this.client.authorizationUrl({
+      url: client.buildAuthorizationUrl(this.config, {
         state,
         scope: 'openid profile email',
         redirect_uri: dto.redirectUri,
@@ -53,25 +42,32 @@ export class OAuthService {
   async callback({ url }: OAuthCallbackDto) {
     try {
       const redirectUri = new URL(url).origin + '/claim/callback';
-      const params = this.client.callbackParams(url);
 
-      if (!params.state || !stateMap.has(params.state)) {
+      if (!stateMap.has(redirectUri)) {
         throw new BadRequestException('Invalid state parameter');
       }
 
-      const stateItem = stateMap.get(params.state);
+      const stateItem = stateMap.get(redirectUri);
       if (!stateItem || stateItem.expiresAt < Date.now()) {
         throw new BadRequestException('Invalid state parameter');
       }
 
-      const tokens = await this.client.oauthCallback(redirectUri, params, { state: stateItem.value });
-      const profile = await this.client.userinfo<GithubProfile>(tokens);
+      const tokens = await client.authorizationCodeGrant(this.config, new URL(redirectUri), {
+        expectedState: stateItem.value,
+      });
+      const profile = await client.fetchUserInfo(this.config, tokens.access_token, tokens.claims()?.sub || '');
+      const login = profile.login as string;
+      const avatarUrl = profile.avatar_Url as string;
 
-      const licenses = await this.database.getSponsorLicenses(profile.login);
+      if (!login || !avatarUrl) {
+        throw new Error('Could not fetch user info');
+      }
+
+      const licenses = await this.database.getSponsorLicenses(login);
 
       return {
-        username: profile.login,
-        imageUrl: profile.avatar_url,
+        username: login,
+        imageUrl: avatarUrl,
         licenses,
       };
     } catch (error: Error | AggregateError | unknown) {
