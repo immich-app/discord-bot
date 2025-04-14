ef581588654acee74ff98766477b7a9f57b9cacd
Daniel Dietzler <36593685+danieldietzler@users.noreply.github.com>
2025-04-14 13:40:50 +0200

    feat: sync focus topics from discord to outline (#248)

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -3,11 +3,13 @@ import { IDatabaseRepository } from 'src/interfaces/database.interface';
 import { IDiscordInterface } from 'src/interfaces/discord.interface';
 import { IFourthwallRepository } from 'src/interfaces/fourthwall.interface';
 import { IGithubInterface } from 'src/interfaces/github.interface';
+import { IOutlineInterface } from 'src/interfaces/outline.interface';
 import { IZulipInterface } from 'src/interfaces/zulip.interface';
 import { DatabaseRepository } from 'src/repositories/database.repository';
 import { DiscordRepository } from 'src/repositories/discord.repository';
 import { FourthwallRepository } from 'src/repositories/fourthwall.repository';
 import { GithubRepository } from 'src/repositories/github.repository';
+import { OutlineRepository } from 'src/repositories/outline.repository';
 import { ZulipRepository } from 'src/repositories/zulip.repository';
 
 export const providers: Provider[] = [
@@ -16,5 +18,6 @@ export const providers: Provider[] = [
   { provide: IDiscordInterface, useClass: DiscordRepository },
   { provide: IFourthwallRepository, useClass: FourthwallRepository },
   { provide: IGithubInterface, useClass: GithubRepository },
+  { provide: IOutlineInterface, useClass: OutlineRepository },
   { provide: IZulipInterface, useClass: ZulipRepository },
 ];
