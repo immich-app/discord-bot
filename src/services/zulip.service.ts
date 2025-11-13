f9b45181c48e9e1e118c69bc69c7280f39fcb4c2
Daniel Dietzler <mail@ddietzler.dev>
2025-11-13 18:32:20 +0100

    fix: holidays, this time for real?

--- src/services/zulip.service.ts
+++ src/services/zulip.service.ts
@@ -3,7 +3,7 @@ import { Cron } from '@nestjs/schedule';
 import { DateTime } from 'luxon';
 import { getConfig } from 'src/config';
 import { Constants } from 'src/constants';
-import { IHolidaysInterface } from 'src/interfaces/holidays.interface';
+import { HolidayDto, IHolidaysInterface } from 'src/interfaces/holidays.interface';
 import { IZulipInterface } from 'src/interfaces/zulip.interface';
 
 @Injectable()
@@ -25,20 +25,18 @@ export class ZulipService {
     const tomorrow = DateTime.now().plus({ days: 1 });
     const holidays = await this.holidays.getHolidays('US', tomorrow.year);
 
-    const holiday = holidays.find(({ date }) => date === tomorrow.toISODate());
+    const isRelevantHoliday = (holiday: HolidayDto) =>
+      holiday.types?.includes('Public') && (holiday.global || holiday.counties?.includes('US-TX'));
+    const holiday = holidays.find((holiday) => holiday.date === tomorrow.toISODate() && isRelevantHoliday(holiday));
 
     if (!holiday) {
       return;
     }
 
-    if (holiday.global) {
-      const content = `Tomorrow is a federal holiday: ${holiday.name}. Most likely there won't be any meetings tomorrow, otherwise Steve will correct me :kekw:`;
-
-      await this.zulip.sendMessage({
-        stream: Constants.Zulip.Streams.FUTOStaff,
-        topic: 'Holidays',
-        content,
-      });
-    }
+    await this.zulip.sendMessage({
+      stream: Constants.Zulip.Streams.FUTOStaff,
+      topic: 'Holidays',
+      content: `Tomorrow is a federal holiday: ${holiday.name}. Most likely there won't be any meetings tomorrow, otherwise Steve will correct me :kekw:`,
+    });
   }
 }
