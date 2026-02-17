8b2d12f5227d08ed4926a79f8310d765583c4e99
Daniel Dietzler <mail@ddietzler.dev>
2026-02-17 12:33:16 +0100

    chore: prepare for eslint 10

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,12 @@
-import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
 
 export default [
   {
@@ -30,10 +24,10 @@ export default [
       '**/yarn.lock',
     ],
   },
-  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended'),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     plugins: {
-      '@typescript-eslint': typescriptEslint,
       'no-relative-import-paths': noRelativeImportPaths,
     },
 
